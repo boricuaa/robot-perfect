a6210534ab5ef8b4c7634e6e2296c575f0f0f43e
Angel sopena <angelsopena@Angels-MacBook-Pro.local>
2019-11-29 15:42:22 -0600

    Updates

---
+++ precache-manifest.9ab3b55f4bbdc52c46c6762b360e7205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfe26a163dbe6a9f1ea177bcb0f6e76",
+    "url": "/robot-perfect/index.html"
+  },
+  {
+    "revision": "27e29b2b31c087a8271f",
+    "url": "/robot-perfect/static/css/main.f6d0668e.chunk.css"
+  },
+  {
+    "revision": "7c57f2a77176e8b6293d",
+    "url": "/robot-perfect/static/js/2.b51d3c26.chunk.js"
+  },
+  {
+    "revision": "27e29b2b31c087a8271f",
+    "url": "/robot-perfect/static/js/main.43b83142.chunk.js"
+  },
+  {
+    "revision": "77cb60a033d447e4a13b",
+    "url": "/robot-perfect/static/js/runtime-main.6b07988a.js"
+  }
+]);
